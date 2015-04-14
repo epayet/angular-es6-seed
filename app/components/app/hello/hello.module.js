090d57d2e8ec24d044d43ca6f4c294dbcc848e81
epayet <jakspinning@gmail.com>
2015-04-14 18:54:54 +0200

    Changed to Hello

--- app/components/app/hello/hello.module.js
+++ app/components/app/hello/hello.module.js
@@ -3,7 +3,7 @@ import HelloController from './hello.controller.js';
 import HelloService from './hello.service.js';
 
 export default angular.module('app.toto', [])
-    .directive('toto', HelloDirective)
+    .directive('hello', HelloDirective)
     .controller('HelloController', HelloController)
     .service('helloService', HelloService)
 ;
